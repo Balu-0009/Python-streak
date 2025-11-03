928e5a326929e7dcfa6553ac7f1952748f43acdd
Balu-0009 <kolabalanagavenkat@gmail.com>
2025-11-03 17:48:41 +0530

    ADD prime problem

--- pythonprograms.py
+++ pythonprograms.py
@@ -6,3 +6,19 @@ if num % 2 == 0:
     print(f"{num} is an Even number.")
 else:
     print(f"{num} is an Odd number.")
+
+2# Program to find the prime numbers or not
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
+
+
